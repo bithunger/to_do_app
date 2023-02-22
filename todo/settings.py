5fd149634efcb003d09617ddddbe9346ee1815b2
bithunger <123397821+bithunger@users.noreply.github.com>
2023-02-23 03:39:59 +0600

    to do

--- todo/settings.py
+++ todo/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'to-do',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
